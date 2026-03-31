8ccf8a78d5f1776741dc24be8e6d70e39cecf51e
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-03-31 18:52:11 +0200

    pari o dispari

--- scripts/function.js
+++ scripts/function.js
@@ -1,1 +1,16 @@
 'use strict'
+
+
+function random_num_to_5(){
+    const random_num = Math.floor((Math.random()*5)+1);
+    return random_num;
+} 
+
+function even_or_odd (numero) {
+    if (numero % 2 === 0) {
+        return 'pari';
+    }else{
+        return 'dispari';
+    }
+    
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,21 @@
 'use strict'
-
+//////////////////////////////////////////////////////////////////////////
+//GIOCO DEL PARI E DISPARI
+/////////////////////////////////////////////////////////////////////////
+let scelta_giocatore = '';
+let numero_giocatore = 0;
+do {
+    scelta_giocatore = prompt('pari o dispari?').toLowerCase().trim(); //scelta tra pari e dispari con il .tolowercase per avere sempre tutto in minuscolo
+    console.log(scelta_giocatore);
+    
+} while ((scelta_giocatore !== 'pari') && (scelta_giocatore !== 'dispari'));
+do {
+    numero_giocatore = parseInt(prompt("Scegli un numero 1-5"));//scelta del tumero tra 1 e 5
+} while (isNaN(numero_giocatore) || (numero_giocatore < 1) || (numero_giocatore > 5));
+const numero_cpu = random_num_to_5(); // calcolo del numero random tra 1 e 5 per la cpu 
+const sum = numero_giocatore + numero_cpu; // somma del numero giocatore e cpu
+const risultato = even_or_odd(sum);// check che la sommma sia pari o dispari
+const vincitore = risultato === scelta_giocatore;//variabile booleana per capire se la scelta del giocatore era giusta o sbagliata
+console.log(`Tu: ${numero_giocatore}, CPU: ${numero_cpu}, Somma: ${sum} (${risultato})`);//log di tutti i valori cosi da fare un controllo sui numeri
+console.log(vincitore ? "Hai vinto!" : "Hai perso!");// sfruttiamo la variabile booleana per determinare se stampare la vittoria o la sconfitta 
 
